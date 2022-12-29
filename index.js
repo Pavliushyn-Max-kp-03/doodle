80e6ef6c7f8c834b18d644d013cb232206a60a1a
pl.max03 <pavliushyn.max.kp03@gmail.com>
2022-12-29 16:23:46 +0200

    work with platform

--- index.js
+++ index.js
@@ -18,6 +18,51 @@ document.addEventListener('DOMContentLoaded', () => {
     let leftTimerId
     let rightTimerId
   
+    class Platform {
+      constructor(newPlatBottom) {
+        this.left = Math.random() * 315
+        this.bottom = newPlatBottom
+        this.visual = document.createElement('div')
+  
+        const visual = this.visual
+        visual.classList.add('platform')
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom + 'px'
+        grid.appendChild(visual)
+      }
+    }
+  
+  
+    function createPlatforms() {
+      for(let i =0; i < platformCount; i++) {
+        let platGap = 600 / platformCount
+        let newPlatBottom = 100 + i * platGap
+        let newPlatform = new Platform (newPlatBottom)
+        platforms.push(newPlatform)
+        console.log(platforms)
+      }
+    }
+  
+    function movePlatforms() {
+      if (doodlerBottomSpace > 200) {
+          platforms.forEach(platform => {
+            platform.bottom -= 4
+            let visual = platform.visual
+            visual.style.bottom = platform.bottom + 'px'
+  
+            if(platform.bottom < 10) {
+              let firstPlatform = platforms[0].visual
+              firstPlatform.classList.remove('platform')
+              platforms.shift()
+              console.log(platforms)
+              score++
+              var newPlatform = new Platform(600)
+              platforms.push(newPlatform)
+            }
+        }) 
+      }
+      
+    }
   
     function start() {
       if (!isGameOver) {
